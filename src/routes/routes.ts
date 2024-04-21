48f4f9a1f286a1cdfc97c0e19d113d3163e8ab4e
Dương Thanh Phụng <20130099@st.hcmuaf.edu.vn>
2024-04-21 17:34:06 +0700

    CartPage

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,16 +1,19 @@
-import HomePage from "../pages/HomePage/HomePage"
+import CartPage from "../pages/Cart/CartPage";
+import HomePage from "../pages/HomePage/HomePage";
 import LoginRegister from "../pages/Users";
 import ForgotPassPage from "../pages/Users/FogotPassPage";
 
 const pathName = {
-    homepage: '/',
-    user: '/user',
-    forgot_pass: '/forgot_pass'
-}
+  homepage: "/",
+  user: "/user",
+  forgot_pass: "/forgot_pass",
+  cart: "/cart",
+};
 
 const routes = [
-        { path: pathName.homepage, element: HomePage },
-        {path: pathName.user, element: LoginRegister},
-        {path: pathName.forgot_pass, element: ForgotPassPage},
-    ]
+  { path: pathName.homepage, element: HomePage },
+  { path: pathName.user, element: LoginRegister },
+  { path: pathName.forgot_pass, element: ForgotPassPage },
+  { path: pathName.cart, element: CartPage },
+];
 export default routes;
